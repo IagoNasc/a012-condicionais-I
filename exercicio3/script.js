bb34416644b2a820723585a1e2b352766f85bd6e
unknown <iagosto.nasc@gmail.com>
2022-09-06 16:08:33 -0300

    Commit A012-condicionais

--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,1 +1,9 @@
-let nacionalidade //= adicione o prompt aqui
+let nacionalidade = prompt("Qual a sua nacionalidade?")
+
+if(nacionalidade === "brasileira" || nacionalidade === "argentina" || nacionalidade === "uruguaia" || nacionalidade === "chilena" || nacionalidade === "colombiana"){
+    console.log("Você é:", nacionalidade);
+}
+else{
+    ("nacionalidade não encontrada")
+}
+
